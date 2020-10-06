0e7412c990744dad9724dcd3691eaa6ad6db5881
Johannes Abendroth <yo-hann@gmx.de>
2020-10-07 00:04:20 +0200

    Change application text

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,6 @@
     v-show="showTyping"
     role="main"
     class="console"
-    @click="onClickConsole"
   >
     <div class="console__wrapper">
       <div class="console__message">
@@ -11,11 +10,11 @@
           <p>
             Hi there,<br>
             My name is <b>Amélie Bonet</b>.^1000 <br>
-            Originally from Paris, I am a type designer and font engineer based in Berlin. <br>
+            Originally from Paris, I am a type designer and font engineer based in Berlin.<br>
             <br>
-            I am a graduate of the École Estienne (MA typography &amp; typeface design), the École des Arts Décoratifs (graphic design) and the University of Reading (MA typeface design).<br>
+            I am a graduate of the École Estienne (DSAA Créationn Typographique), the École Nationale Supérieure des Arts Décoratifs (Post-Diplôme Édition) and the University of Reading (MA Typeface Design).<br>
             <br>
-            I drawn letters and produced fonts for companies such as LuxTypo (Los Angeles), Dalton Maag (London) and Monotype (Berlin).<br>
+            I have drawn letters and produced fonts for companies such as LuxTypo (Los Angeles), Dalton Maag (London) and Monotype (Berlin).<br>
             <br>
             I am now free^1000lancing.<br>
             I have recently been heading the design and production of the new Noto Sans Oriya for Google fonts.<br>
@@ -23,7 +22,17 @@
             I am working with Glyphs.<br>
             <br>
             I would be happy to join your team at Brand New Type.<br>
-            Click here to download my resume or press any key to see more of my work.
+            <br>
+            <button class="button">
+              Resume
+            </button>
+            | and
+            <button
+              class="button"
+              @click="onClickConsole"
+            >
+              Portfolio
+            </button>
           </p>
         </div>
         <span id="typed" />
@@ -101,5 +110,10 @@ export default {
     position: relative;
     font-size: calc(14px + (26 - 14) * ((100vw - 300px) / (1600 - 300)));
   }
+}
+
+.button {
+  text-transform: uppercase;
+  letter-spacing: 0.2em;
 }
 </style>
